1f8348cefd8b38b38a58aaadb78743faa4724dae
munisaazim <munisa2000azim@gmail.com>
2023-02-01 17:08:37 +0500

    Adding locators

---
+++ Lab/Lecture7/iTechArt.Lab7.Lecture/src/test/java/org/example/Configurator.java
@@ -0,0 +1,9 @@
+package org.example;
+
+
+import org.openqa.selenium.devtools.v107.browser.Browser;
+
+public class Configurator {
+
+
+}
---
+++ Lab/Lecture7/iTechArt.Lab7.Lecture/src/test/java/org/example/Test.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Test {
+    SeleniumBasic object = new SeleniumBasic();
+
+    @org.junit.Test
+    public void test() throws InterruptedException {
+        object.setuo();
+        object.Smoke();
+    }
+}
