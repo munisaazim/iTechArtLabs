7323aeaf42f5873aed8dc6bbb0286fb90d7b33cd
munisaazim <munisa2000azim@gmail.com>
2023-02-22 22:22:19 +0500

    Improving task5

--- Labs_JavaFiles_Xml/Lecture5/iTechArt.Lab5.HW/src/test/java/org/example/RunAllTests.java
+++ Labs_JavaFiles_Xml/Lecture5/iTechArt.Lab5.HW/src/test/java/org/example/RunAllTests.java
@@ -1,14 +1,29 @@
 package org.example;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class RunAllTests {
+    private WebDriver driver;
+    @BeforeTest
+    public void settingUp(){
+        this.driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
     @Test
     public void Test(){
-        TaskOne object = new TaskOne();
+        TaskOne object = new TaskOne(driver);
         object.enter();
         object.clickOnServices();
         object.clickOnContact();
     }
+    @AfterTest
+    public void quitDriver(){
+        driver.quit();
+    }
+
 }
 
--- Labs_JavaFiles_Xml/Lecture5/iTechArt.Lab5.HW/src/test/java/org/example/TaskOne.java
+++ Labs_JavaFiles_Xml/Lecture5/iTechArt.Lab5.HW/src/test/java/org/example/TaskOne.java
@@ -1,29 +1,33 @@
 package org.example;
 
+import org.apache.commons.lang3.NotImplementedException;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.openqa.selenium.WebElement;
 
 public class TaskOne {
-    WebDriver driver = new ChromeDriver();
-    @Test
+    private WebDriver driver;
+    protected String expectedFaxNumber, actualFaxNumber;
+    protected WebElement actualFaxNumberElement;
+    public TaskOne(WebDriver driver){
+        this.driver = driver;
+    }
     public void enter(){
         String webName = "https://uzmarketing.com/";
         driver.get(webName);
-        Assert.assertEquals(driver.getCurrentUrl(), webName);
     }
     public void clickOnServices(){
         driver.findElement(By.xpath("//*[@id = \"menu-item-1079\"]/a")).click();
-        Assert.assertEquals(driver.findElement(By.xpath("//a[@href = \"/services/web-development/\"]")).getText(),"  WEB DEVELOPMENT");
     }
     public void clickOnContact(){
         driver.findElement(By.xpath("//*[@id = \"menu-item-1077\"]/a")).click();
-        Assert.assertEquals(driver.findElement(By.xpath("(//*[@class = \"address\"]//span)[4]")).getText(), "Fax: (800) 217-9962");
-    }
-
-    public void quit(){
-        driver.quit();
+        expectedFaxNumber = "Fax: (800) 217-9962";
+        actualFaxNumberElement = driver.findElement(By.xpath("(//*[@class = \"address\"]//span)[4]"));
+        if(actualFaxNumberElement.getText().equals(expectedFaxNumber)){
+            System.out.println("The fax number:" + expectedFaxNumber);
+        }
+        else {
+            throw new NotImplementedException();
+        }
     }
 }
