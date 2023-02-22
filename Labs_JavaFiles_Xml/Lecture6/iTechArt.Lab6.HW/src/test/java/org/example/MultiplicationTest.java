383f7bb99076a2cf0d9978e7c656eaabfa309360
munisaazim <munisa2000azim@gmail.com>
2023-02-22 23:04:20 +0500

    Refactoring Lab6

--- Labs_JavaFiles_Xml/Lecture6/iTechArt.Lab6.HW/src/test/java/org/example/MultiplicationTest.java
+++ Labs_JavaFiles_Xml/Lecture6/iTechArt.Lab6.HW/src/test/java/org/example/MultiplicationTest.java
@@ -3,12 +3,15 @@ package org.example;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 public class MultiplicationTest {
     @Test
     public void checkMultiplication(){
         Multiplication multiplication = new Multiplication();
-        double num1 = 10, num2 = 2;
-        double result = multiplication.multipleCalculation(num1,num2);
-        Assert.assertEquals(result, 20.0);
+        double numberOne = 10, numberTwo = 2;
+        double result = multiplication.multipleCalculation(numberOne,numberTwo);
+        assertThat("Multiplication of two numbers is correct", result, is(20.0));
     }
 }
