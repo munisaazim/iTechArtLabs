2ece13ecb0bcfba4afd1b9f66506f7e12304696a
munisaazim <munisa2000azim@gmail.com>
2023-02-24 11:08:12 +0500

    Refactoring LAb 9

--- Labs_JavaFiles_Xml/Lecture9/iTechArt.Lab9.HW_New/src/test/java/namespace/tests/TC1.java
+++ Labs_JavaFiles_Xml/Lecture9/iTechArt.Lab9.HW_New/src/test/java/namespace/tests/TC1.java
@@ -9,30 +9,31 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class TC1 {
-     static WebDriver driver;
+    static WebDriver driver;
     private String mainPageUrl = "https://catalog.onliner.by/";
+
     @BeforeTest
-    public void setUp(){
+    public void setUp() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get(mainPageUrl);
     }
+
     @Test
-    public void runTC1(){
+    public void runTC1() {
         MainPage mainPage = new MainPage(driver);
         mainPage.clickOnMobileLink();
         ChoosePhonesPage choosePhonesPage = new ChoosePhonesPage(driver);
         choosePhonesPage.clickOnAppleAndHONOR();
         choosePhonesPage.clickOnProductOneAndProductTwo();
         choosePhonesPage.clickOnLinkCompareProducts();
-        CompareProductsPage compareProductsPage= new CompareProductsPage(driver);
+        CompareProductsPage compareProductsPage = new CompareProductsPage(driver);
         compareProductsPage.assertCompareProductsPage();
 
     }
-    /*
+
     @AfterTest
-    public void quit(){
+    public void quit() {
         TC1.driver.quit();
     }
-     */
 }
